954122dc386142f3eed81efefb9f7b3ad0a90003
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-14 22:33:13 +0800

    add event listener in search bar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,10 +2,37 @@ import React, { Component } from "react";
 import "./SearchBar.css";
 
 export default class SearchBar extends Component {
+	state = {
+		searchVideo: "",
+	};
+
+	// for input when it change
+	onInputChange = (e) => {
+		this.setState({ searchVideo: e.target.value });
+	};
+
+	// for form when user clicked or enter submit
+	onFormSubmit = (e) => {
+		e.preventDefault();
+
+		console.log("submittted");
+
+		// TODO: make sure we call the callback from the parent component
+	};
+
 	render() {
 		return (
-			<div className="searchBar">
-				<h2>SearchBar Component</h2>
+			<div className="searchBar ui segment">
+				<form className="ui form" onSubmit={this.onFormSubmit}>
+					<div className="field">
+						<input
+							type="text"
+							placeholder="Search Video"
+							value={this.state.searchVideo}
+							onChange={this.onInputChange}
+						/>
+					</div>
+				</form>
 			</div>
 		);
 	}
